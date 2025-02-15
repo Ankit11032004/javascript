87361606ee1c7d97305dd2f3243d58970a4917a4
Ankit11032004 <159641091+Ankit11032004@users.noreply.github.com>
2025-02-15 17:50:39 +0000

    datatype

--- javascriptcode/02_datatype.js
+++ javascriptcode/02_datatype.js
@@ -1,2 +1,18 @@
 "use strict";//treat all JS code as newer version
-alert("hello")
+console.log(3+3)
+console.log("hitesh")
+let name="hitesh"
+let age=18
+let isLoggedIn=false
+//number =>2 to power 53
+//bigint
+//string=>""
+//boolean=>true/false
+//null=>stanndalone value
+//undefined =>
+//symbol=>unique
+//object
+console.log(typeof "hitesh");
+console.log(typeof null);//object
+console.log(typeof undefined);//undefined
+
