63bd07d1e33717baa119daad7d96ad73bfc9413c
Ankit11032004 <159641091+Ankit11032004@users.noreply.github.com>
2025-02-22 11:53:47 +0000

    conversion

---
+++ javascriptcode/basic/01_variables.js
@@ -0,0 +1,14 @@
+const accountId=144553
+let accountEmial="abc@google.com"
+var accountPassword="12345"
+accountCity="jaipur"
+let accountState;
+// accountId=2//not allowed
+/*prefer not to use var because of issue in bloack scope an function scope */
+
+accountEmail="hc@hc.com"
+accountPassword="21212121"
+accountCity="Bengaluru"
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+
