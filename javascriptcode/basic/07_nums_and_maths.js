6a8facb71dcdee1eda3df1297d34b0be93d6c7f7
Ankit11032004 <159641091+Ankit11032004@users.noreply.github.com>
2025-03-03 02:41:49 +0000

    nums

---
+++ javascriptcode/basic/07_nums_and_maths.js
@@ -0,0 +1,18 @@
+const score=400
+console.log(score);
+
+const balance= new Number(100)
+console.log(balance);
+
+console.log(balance.toString().length);
+console.log(balance.toFixed(2));
+
+const otherNumber=12.8695
+console.log(otherNumber.toFixed(2));
+console.log(otherNumber.toPrecision(3));
+
+
+//node javascriptcode/basic/07_nums_and_maths.js
+
+
+
