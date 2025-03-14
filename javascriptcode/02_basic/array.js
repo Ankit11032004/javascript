7ae70860087b25f7c4428258bcb05b6e40fb07b9
Ankit11032004 <159641091+Ankit11032004@users.noreply.github.com>
2025-03-14 16:50:48 +0000

    addarray

--- javascriptcode/02_basic/array.js
+++ javascriptcode/02_basic/array.js
@@ -23,6 +23,41 @@ console.log(splicedArr);
 console.log(arr);
 arr.splice(2, 0, 'a', 'b');  // Inserts 'a' and 'b' at index 2
 console.log(arr); 
+const nums=[1,2,3,4,5,6];
+const squared=nums.map(num=>num*num);
+console.log(squared);
+const evens=nums.filter(num => num%2==0);
+console.log(evens);
+const sum=nums.reduce((acc,num)=>acc +num,0);
+console.log(sum);
+const[first,second,...rest]=[10,20,30,40];
+console.log(first,second,rest);
+const obj={a:1,b:2};
+const newObj={...obj,c:3};
+console.log(newObj);
+function multiplyBy(factor){
+    return function(num){
+        return num*factor;
+    };
+}
+const double=multiplyBy(2);
+const triple=multiplyBy(3);
+console.log(double(5));
+console.log(triple(2));
+class Stack{
+    constructor(){
+        this.items=[];
+    }
+    push(element){
+        this.items.push(element);
+    }
+}
+let stack=new Stack();
+stack.push(20);
+stack.push(10);
+console.log(stack.items);
+
+
 
 
 
